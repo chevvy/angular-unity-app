458c1f8fe47f6ce0a082301cacd6cba602673088
Vincent Chavanel Jobin <vchavaneljobin@petalmd.com>
2020-08-17 16:27:12 -0400

    Add - Bare minimum static implementation with fps template

--- src/app/fps-template/fps-template.component.ts
+++ src/app/fps-template/fps-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var createUnityInstance;
 
 @Component({
   selector: 'app-fps-template',
@@ -10,6 +11,52 @@ export class FpsTemplateComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const buildUrl = 'assets/Games/FPS';
+    const loaderUrl = buildUrl + '/buildWeb.loader.js';
+    const config = {
+      dataUrl: buildUrl + '/buildWeb.data',
+      frameworkUrl: buildUrl + '/buildWeb.framework.js',
+      codeUrl: buildUrl + '/buildWeb.wasm',
+      streamingAssetsUrl: 'StreamingAssets',
+      companyName: 'DefaultCompany',
+      productName: 'testPlatformer',
+      productVersion: '0.1',
+      devicePixelRatio: null,
+    };
 
+    const container = document.querySelector('#unity-container');
+    const canvas = document.querySelector('#unity-canvas');
+    const loadingBar = document.querySelector('#unity-loading-bar');
+    const progressBarFull = document.querySelector('#unity-progress-bar-full');
+    const fullscreenButton = document.querySelector('#unity-fullscreen-button');
+
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+      container.className = 'unity-mobile';
+      config.devicePixelRatio = 1;
+    } else {
+      // @ts-ignore
+      canvas.style.width = '960px';
+      // @ts-ignore
+      canvas.style.height = '600px';
+    }
+    // @ts-ignore
+    loadingBar.style.display = 'block';
+
+    const script = document.createElement('script');
+    script.src = loaderUrl;
+
+    createUnityInstance(canvas, config, (progress) => {
+      // @ts-ignore
+      progressBarFull.style.width = 100 * progress + '%';
+    }).then((unityInstance) => {
+      // @ts-ignore
+      loadingBar.style.display = 'none';
+      // @ts-ignore
+      fullscreenButton.onclick = () => {
+        unityInstance.SetFullscreen(1);
+      };
+    }).catch((message) => {
+      alert(message);
+    });
+  }
 }
